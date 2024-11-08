8d12020849220dd9f862196d66e7807657751bd1
alumno <alumno@pc-iada50.uacj.mx>
2024-11-08 09:18:51 -0700

    Primer commit

---
+++ proyecto_final_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proyecto_final_ios
+//
+//  Created by alumno on 11/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ proyecto_final_iosUITests/proyecto_final_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  proyecto_final_iosUITestsLaunchTests.swift
+//  proyecto_final_iosUITests
+//
+//  Created by alumno on 11/8/24.
+//
+
+import XCTest
+
+final class proyecto_final_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
