ff5bafe9cd49daff8dfdb1b16f82b30c42c17332
alumno <alumno@PC-V202-05.uacj.mx>
2024-11-22 12:29:53 -0700

    Agregado extra de los movimientos

---
+++ proyecto_final_ios/ListaDeMovimeintosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListaDeMovimeintosViewController.swift
+//  proyecto_final_ios
+//
+//  Created by alumno on 11/22/24.
+//
+
+import UIKit
+
+class ListaDeMovimeintosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
