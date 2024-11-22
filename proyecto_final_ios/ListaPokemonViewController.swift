88d6c775ee17575d4f42fd6cf24c8ac12eadc1a1
alumno <alumno@PC-V202-05.uacj.mx>
2024-11-22 10:58:36 -0700

    Actualizacion barra busqueda

--- proyecto_final_ios/ListaPokemonViewController.swift
+++ proyecto_final_ios/ListaPokemonViewController.swift
@@ -16,6 +16,7 @@ class ListaPokemonViewController: UIViewController {
     var pokemonManager = PokemonManager()
     var pokemones_array : [Datos] = []
     var pokemon_seleccionado: Datos?
+    var pokemon_filtro_busqueda : [Datos] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +25,44 @@ class ListaPokemonViewController: UIViewController {
         
         pokemonManager.delegado = self
         
+        barra_buscadora_pokemones.delegate = self
+        
         tabla_pokemones.delegate = self
         tabla_pokemones.dataSource = self
         
         //Mostramos la lista de los pokemones
         pokemonManager.verPokemon()
-            
+        
+        pokemon_filtro_busqueda = pokemones_array
         }
     }
+
+//Codigo para la barra buscadora
+extension ListaPokemonViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        pokemon_filtro_busqueda = []
+        
+        if searchText == "" {
+            pokemon_filtro_busqueda = pokemones_array
+        } else{
+            for poke in pokemones_array{
+                if poke.name.lowercased().contains(searchText.lowercased()){
+                    pokemon_filtro_busqueda.append(poke)
+                }
+            }
+        }
+        
+        self.tabla_pokemones.reloadData()
+    }
+}
 //Pokemon delegado
 extension ListaPokemonViewController: pokemonManagerDelegado{
     func mostrarListaPokemon(lista: [Datos]) {
         pokemones_array = lista
         
         DispatchQueue.main.async {
+            self.pokemon_filtro_busqueda = self.pokemones_array
             self.tabla_pokemones.reloadData()
         }
     }
@@ -45,16 +70,16 @@ extension ListaPokemonViewController: pokemonManagerDelegado{
 //Codigo tablas
 extension ListaPokemonViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pokemones_array.count
+        return pokemon_filtro_busqueda.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celda = tabla_pokemones.dequeueReusableCell(withIdentifier: "celda", for: indexPath) as! CeldaPokemonTableViewCell
-        celda.nombrePokemon.text = pokemones_array[indexPath.row].name
-        celda.ataquePokemon.text = "Attack: \(pokemones_array[indexPath.row].attack)"
-        celda.defensaPokemon.text = "Defense: \(pokemones_array[indexPath.row].defense)"
+        celda.nombrePokemon.text = pokemon_filtro_busqueda[indexPath.row].name
+        celda.ataquePokemon.text = "Attack: \(pokemon_filtro_busqueda[indexPath.row].attack)"
+        celda.defensaPokemon.text = "Defense: \(pokemon_filtro_busqueda[indexPath.row].defense)"
         
-        if let urlString = pokemones_array[indexPath.row].imageUrl as? String{
+        if let urlString = pokemon_filtro_busqueda[indexPath.row].imageUrl as? String{
             if let imagenUrl = URL(string: urlString){
                 DispatchQueue.global().async {
                     guard let imagenData = try? Data(contentsOf: imagenUrl) else{
@@ -73,7 +98,7 @@ extension ListaPokemonViewController: UITableViewDelegate, UITableViewDataSource
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        pokemon_seleccionado = pokemones_array[indexPath.row]
+        pokemon_seleccionado = pokemon_filtro_busqueda[indexPath.row]
         performSegue(withIdentifier: "verDatosPokemon", sender: self)
         tabla_pokemones.deselectRow(at: indexPath, animated: true)
     }
